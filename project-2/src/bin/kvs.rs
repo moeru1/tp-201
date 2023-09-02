e42eeab03c70e052a548d8fe5e93177e92c6ab96
moeru <juan.cisneros315@gmail.com>
2023-09-02 06:52:30 -0500

    project-2 WIP: step 5

--- project-2/src/bin/kvs.rs
+++ project-2/src/bin/kvs.rs
@@ -1,6 +1,8 @@
 use clap::{Parser, Subcommand};
 use kvs::{KvStore, Result, KeyNotFound};
 use std::path::PathBuf;
+use std::process::{ExitCode, Termination};
+use anyhow::anyhow;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -26,14 +28,30 @@ enum CliCommands {
     }
 }
 
-fn main() -> Result<()>{
+//struct AppResult(Result<()>);
+//
+//impl Termination for AppResult {
+//    fn report(self) -> ExitCode {
+//        match self {
+//            Ok(_) => ExitCode::SUCCESS,
+//            Err(e) => ExitCode::FAILURE
+//        }
+//    }
+//}
+
+fn main() -> Result<()> {
     let cli = Cli::parse();
-    run(cli)
+    let result = run(cli);
+    if result
+        .as_ref()
+            .is_err_and(|e| e.downcast_ref::<KeyNotFound>().is_some()) {
+        std::process::exit(1)
+    }
+    result
 }
 
 fn run(cli: Cli) -> Result<()> {
-    let mut kv_store = KvStore::open(PathBuf::from("kv_store.txt"))?;
-
+    let mut kv_store = KvStore::open(PathBuf::from("."))?;
 
     match cli.command {
         CliCommands::Set{key, value} => {
@@ -57,7 +75,7 @@ fn run(cli: Cli) -> Result<()> {
             match e.downcast_ref::<KeyNotFound>() {
                 Some(_) => {
                     println!("Key not found");
-                    Ok(())
+                    Err(e)
                 }
                 None => Err(e)
             }
--- project-2/src/lib.rs
+++ project-2/src/lib.rs
@@ -8,8 +8,8 @@ use anyhow::anyhow;
 use std::{
     collections::HashMap,
     path::PathBuf,
-    io::{BufReader, BufRead, Write},
-    fs::File,
+    io::{BufReader, BufRead, Write, Seek, SeekFrom},
+    fs::{File, self},
     fmt,
 };
 use serde::{Serialize, Deserialize};
@@ -44,14 +44,24 @@ impl fmt::Display for KeyNotFound {
 impl KvStore {
     /// Creates an empty kv_store
     pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        let log_file_name = "kv_store.log";
+        let index_file_name = "index.idx";
+
         let mut kv_memory_map = HashMap::new();
 
         let path = path.into();
-        let path = PathBuf::into_os_string(path.clone());
-        if let Ok(log_file) = File::open(path.clone()) {
+        //let path = PathBuf::into_boxed_path(path.clone());
+        if !path.is_dir() {
+            return Err(anyhow!("{:?} needs to be a directory!", path));
+        }
 
+
+        /*Replay log file***********************************************/
+        let mut path_log_file = path.clone();
+        path_log_file.push(log_file_name);
+        
+        if let Ok(log_file) = File::open(path_log_file.clone()) {
             let log_file_reader = BufReader::new(log_file);
-
             for line in log_file_reader.lines() {
                 let line = line?;
                 let command: LogCommands = serde_json::from_str(&line)?;
@@ -65,14 +75,14 @@ impl KvStore {
                 }
             }
         }
-        
-        println!("creating file");
+        /************************************************************/
+
         let log_file = File::options()
             .read(true)
             .write(true)
             .append(true)
             .create(true)
-            .open(path)?;
+            .open(path_log_file)?;
 
 
         Ok( KvStore { 
@@ -107,12 +117,14 @@ impl KvStore {
     /// if the key was previously in the key-value store.
     pub fn remove(&mut self, key:String) -> Result<()> {
         let rm_cmd = serde_json::to_string(&Rm(key.clone()))?;
-        write!(&self.log_file, "{}\n", rm_cmd)?;
         //self.log_file.write(rm_cmd.as_bytes())?;
         //send error if key doesnt exists
         match self.kv_memory_map.remove(&key) {
             None => Err(anyhow!(KeyNotFound)),
-            Some(_) => Ok(())
+            Some(_) => {
+                write!(&self.log_file, "{}\n", rm_cmd)?;
+                Ok(())
+            }
         }
     }
 }
